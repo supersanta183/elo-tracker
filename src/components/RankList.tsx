741cc1840c67d00555370108b98c38b337098100
au583665 <klemmensen11@gmail.com>
2023-05-11 20:44:56 +0200

    fixed rankings tab

--- src/components/RankList.tsx
+++ src/components/RankList.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { IPlayer } from '../../typings'
-import RankListValues from './RankListValues'
 
 interface props {
     players: IPlayer[];
@@ -10,31 +9,35 @@ interface props {
     gamesPlayed: string;
 }
 
-const RankList: React.FC<props> = ({players, rank, soloRating, teamRating, gamesPlayed}) => {
-  return (
-    <div className='overflow-x-auto'>
-        <table className='table w-full'>
-          <thead>
-            <tr>
-              <th>Rank</th>
-              <td>Player</td>
-              <td>solo rating</td>
-              <td>team rating</td>
-              <td>games played</td>
-            </tr>
-          </thead>
-          <tbody>
-            {players.map((player) => {
-              return (
-                <div key={player.id}>
-                    <RankListValues player={player} rank={player[rank]} soloRating={player[soloRating]} teamRating={player[teamRating]} gamesPlayed={player[gamesPlayed]} />
-                </div>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
-  )
+const RankList: React.FC<props> = ({ players, rank, soloRating, teamRating, gamesPlayed }) => {
+    return (
+        <div className='overflow-x-auto'>
+            <table className='table w-full'>
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <td>Player</td>
+                        <td>solo rating</td>
+                        <td>team rating</td>
+                        <td>games played</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {players.map((player) => {
+                        return (
+                            <tr key={player.id}>
+                                <th>{player[rank]}</th>
+                                <td>{player.name}</td>
+                                <td>{player[soloRating]}</td>
+                                <td>{player[teamRating]}</td>
+                                <td>{player[gamesPlayed]}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default RankList
--- src/pages/Bordfodbold/index.tsx
+++ src/pages/Bordfodbold/index.tsx
@@ -1,9 +1,8 @@
 import { Inter } from 'next/font/google'
 import React, { useEffect, Component } from 'react'
 import { IPlayer } from '../../../typings'
-import handleAddPlayer, {handleFetchPlayers} from '@/common/handlePlayerEmilio183'
+import handleAddPlayer, { handleFetchPlayers } from '@/common/handlePlayerEmilio183'
 import { useState } from 'react'
-import RankListValues from '@/components/RankListValuesEmilio183'
 import RankList from '@/components/RankListEmilio183'
 
 
@@ -20,7 +19,7 @@ export default function Home() {
     const data: IPlayer[] = await handleFetchPlayers()
     data.sort((a, b) => ( //sort players by ranks in ascending order
       a.bordfodboldRank - b.bordfodboldRank
-  ))
+    ))
     setPlayers(data)
   }
 
